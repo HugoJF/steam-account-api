71c5439b6d05a63ece884235b0bf2867bf708451
Hugo Jeller <hugo_jeller@hotmail.com>
2020-01-02 02:58:34 -0400

    added multiple ecosystems for different environments

--- ecosystem.staging.config.js
+++ ecosystem.staging.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
-        name: 'steam-account-api',
-        cwd: '.',
+        name: 'steam-account-api-staging',
+        cwd: './build',
         script: 'index.js',
 
         // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
